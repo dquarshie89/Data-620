a9a292abdd554698727904f22a118ae868c4ec18
David Q <Poster89!>
2018-12-20 00:49:01 -0500

    update plot

--- final2.py
+++ final2.py
@@ -1,12 +1,13 @@
 import pandas as pd
 #Import TfIdfVectorizer from scikit-learn
 from sklearn.feature_extraction.text import TfidfVectorizer
+# Import linear_kernel
+from sklearn.metrics.pairwise import linear_kernel
+
+df = pd.read_csv('https://raw.githubusercontent.com/dquarshie89/Data-620/master/movie_plots.csv')
 
 
-url = pd.read_csv('https://raw.githubusercontent.com/dquarshie89/Data-620/master/movie_plots.csv')
-
-
-df = url.sample(5000)
+#df = url.sample(1000)
 #Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
 tfidf = TfidfVectorizer(stop_words='english')
 
@@ -18,3 +19,31 @@ tfidf_matrix = tfidf.fit_transform(df['Plot'])
 
 #Output the shape of tfidf_matrix
 tfidf_matrix.shape
+
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+#Construct a reverse map of indices and movie titles
+indices = pd.Series(df.index, index=df['Title']).drop_duplicates()
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    # Get the index of the movie that matches the title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return df['Title'].iloc[movie_indices]
+
+
+get_recommendations('23')
